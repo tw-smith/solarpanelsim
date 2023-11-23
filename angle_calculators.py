c4579e83ecd3c1281cb2d9dc64e7dcf41151795b
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-11-23 10:51:41 +0000

    adds angle calculators
    We will need to calculate solar longitude, beta angle and true longitude of the spacecraft to work out the effective panel area of the solar panel. These angles are tightly couple so group all the calculations together in an AngleCalculators class.

---
+++ angle_calculators.py
@@ -0,0 +1,31 @@
+import math
+from org.orekit.time import AbsoluteDate
+
+
+class AngleCalculator:
+    def __init__(self, spacecraft_state, keplerian_orbit): # TODO can we get RAAN from spacecraft state rather than having to import kep orbit?
+        orbit = keplerian_orbit
+        self.raan = orbit.getRightAscensionOfAscendingNode()
+        self.inclination = orbit.getI()
+        self.arg_of_periapsis = orbit.getPerigeeArgument()
+        self.true_anomaly = spacecraft_state.getLv()
+        self.obliquity = math.radians(23.45)
+        seconds_since_J2000 = spacecraft_state.getDate().durationFrom(AbsoluteDate.J2000_EPOCH)
+        self.days_since_J2000 = seconds_since_J2000 / 86400
+
+    def get_sun_longitude(self):
+        L = (280.466 + 0.9856474*self.days_since_J2000) % 360
+        g = (357.528 + 0.9856003*self.days_since_J2000) % 360
+        sun_longitude_degrees = L + 1.915*math.sin(math.radians(g)) + 0.02*math.sin(math.radians(2*g))
+        return math.radians(sun_longitude_degrees)
+
+    def get_beta_angle(self):
+        sun_longitude = self.get_sun_longitude()
+        return math.asin(
+            math.cos(sun_longitude)*math.sin(self.raan)*math.sin(self.inclination) -
+            math.cos(sun_longitude)*math.cos(self.obliquity)*math.cos(self.raan)*math.sin(self.inclination) +
+            math.sin(sun_longitude)*math.sin(self.obliquity)*math.cos(self.inclination)
+        )
+
+    def get_satellite_true_longitude(self):
+        return self.get_sun_longitude() + self.raan + self.true_anomaly
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import orekit.pyhelpers
 from org.orekit.time import AbsoluteDate, TimeScalesFactory
 
 from orbit_propagator import OrbitCreator, OrbitPropagator
+from angle_calculators import AngleCalculator
 
 if not os.path.isfile('/orekit-data.zip'):
     orekit.pyhelpers.download_orekit_data_curdir()
@@ -23,7 +24,7 @@ omega = 0.0
 raan = 0.0
 initial_lv = 0.0
 initialDate = AbsoluteDate(2023, 3, 21, 12, 0, 0.0, utc)
-finalDate = AbsoluteDate(2023, 4, 1, 12, 0, 0.0, utc) # TODO input checks e.g. is initialDate before final date, apogee and perigee etc
+finalDate = AbsoluteDate(2023, 3, 25, 12, 0, 0.0, utc) # TODO input checks e.g. is initialDate before final date, apogee and perigee etc
 timeStep = float(0.05*3600)
 
 orbit = OrbitCreator(
@@ -37,5 +38,9 @@ orbit = OrbitCreator(
 ).get_orbit()
 
 print(orbit)
+state_history = OrbitPropagator(orbit, initialDate, finalDate, timeStep).propagate_orbit()
+angle_calculator = AngleCalculator(state_history[8], orbit)
+print(angle_calculator.get_beta_angle())
+print(angle_calculator.get_sun_longitude())
+print(angle_calculator.get_satellite_true_longitude())
 
-print(OrbitPropagator(orbit, initialDate, finalDate, timeStep).propagate_orbit())
